d4e25a84ab45e0dfbaa23bd04adcb56dd50a0002
Zachary Boelter <zboelter@gmail.com>
2021-10-04 16:09:01 -0500

    images

--- server.js
+++ server.js
@@ -49,25 +49,7 @@ app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 // ===========================================================
 app.get('/products/seed', (req, res) => {
   Product.create(
-    [
-      {
-        name: 'Beans',
-        description: 'A small pile of beans. Buy more beans for a big pile of beans.',
-        img: 'https://imgur.com/LEHS8h3.png',
-        price: 5,
-      }, {
-        name: 'Bones',
-        description: 'It\'s just a bag of bones.',
-        img: 'https://imgur.com/dalOqwk.png',
-        price: 25,
-      }, {
-        name: 'Bins',
-        description: 'A stack of colorful bins for your beans and bones.',
-        img: 'https://imgur.com/ptWDPO1.png',
-        price: 7000,
-      }
-    ]
-    ,
+    
     (error, data) => {
       res.redirect('/products');
     }
